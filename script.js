b19cdfec418edcca27de36424c6f2ad3fa2af4b4
Sylverdrake <126032225+Sylverdrake@users.noreply.github.com>
2023-06-01 18:46:02 -0400

    Add files via upload
    Looks pretty! Does what it's supposed to! Next commit will make it better

---
+++ script.js
@@ -0,0 +1,49 @@
+let job = {
+    warrior: {
+        attributes: [
+            "Strong",
+            "Lots of Health",
+            "Uses Swords and Axes",
+            "Gets a cape"
+        ]
+    },
+
+    wizard: {
+        attributes: [
+            "Smart",
+            "Makes Explosions with Magic",
+            "Uses Staves and Wands",
+            "Dude it's Magic what else do you want"
+        ]
+    },
+
+    thief: {
+        attributes: [
+            "Sneaky",
+            "No one sees them",
+            "Uses Daggers and Bows",
+            "Can steal from the rich"
+        ]
+    },
+
+    priest: {
+        attributes: [
+            "Saintly",
+            "Has the power of god AND anime",
+            "Uses Maces and Staves",
+            "Can destroy undead with holy magic"
+        ]
+    }
+}
+
+const chooseClass = (hero) => {
+    if(hero === "warrior"){
+        console.log(job.warrior.attributes);
+    } else if (hero === "wizard"){
+        console.log(job.wizard.attributes);
+    } else if(hero === "thief"){
+        console.log(job.thief.attributes);
+    } else {
+        console.log(job.priest.attributes);
+    }
+}
